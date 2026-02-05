18a55695085a6e233443e3de338a7124d616275e
Kartik Garg <gargkartik74@gmail.com>
2026-02-05 23:16:33 +0530

    ci3

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,3 +1,4 @@
+import "@rushstack/eslint-patch/modern-module-resolution";
 import nextConfig from "eslint-config-next";
 import coreWebVitals from "eslint-config-next/core-web-vitals.js";
 import typescript from "eslint-config-next/typescript.js";
